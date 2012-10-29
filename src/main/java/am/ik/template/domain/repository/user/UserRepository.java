faa446d7b97cd151bb7e07cd0d548392194261f0
Toshiaki Maki <makingx@gmail.com>
2012-10-30 04:55:00 +0900

    removed unnecessary code...

--- src/main/java/am/ik/template/domain/repository/user/UserRepository.java
+++ src/main/java/am/ik/template/domain/repository/user/UserRepository.java
@@ -9,11 +9,6 @@ import org.springframework.data.repository.query.Param;
 import am.ik.template.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-    @Override
-    User findOne(@Param("id")
-    Integer id);
-
     @Query("SELECT x FROM User x WHERE x.name LIKE :name")
     Page<User> findByNameLike(@Param("name")
     String name, Pageable page);
